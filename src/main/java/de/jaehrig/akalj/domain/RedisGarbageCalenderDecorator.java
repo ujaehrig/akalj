c0d75c12cf754201cb6c77fe6b5c5de9f1a790de
Ulf Jährig <ulf.jaehrig@tipico.com>
2023-02-24 17:55:27 +0100

    add expiration to redis key

--- src/main/java/de/jaehrig/akalj/domain/RedisGarbageCalenderDecorator.java
+++ src/main/java/de/jaehrig/akalj/domain/RedisGarbageCalenderDecorator.java
@@ -3,9 +3,12 @@ package de.jaehrig.akalj.domain;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.time.Duration;
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.Collection;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -45,7 +48,7 @@ public class RedisGarbageCalenderDecorator implements GarbageCalendar {
         } catch (JsonProcessingException e) {
             LOGGER.info("Read failed", e);
         }
-            return Optional.empty();
+        return Optional.empty();
     }
 
     private void writeToRedis(final String street, final String number, final Collection<CalendarEntry> calendarEntries) {
@@ -53,9 +56,26 @@ public class RedisGarbageCalenderDecorator implements GarbageCalendar {
             String key = key(street, number);
             String value = objectMapper.writeValueAsString(calendarEntries);
             redisTemplate.opsForValue().set(key, value);
+
+            long expiration = calcExpiration(calendarEntries).getSeconds();
+            LOGGER.debug("Set expiration to {} seconds", expiration);
+            redisTemplate.expire(key, expiration, TimeUnit.SECONDS);
         } catch (JsonProcessingException exc) {
             LOGGER.warn("Saving failed", exc);
         }
+    }
+
+    private Duration calcExpiration(final Collection<CalendarEntry> calendarEntries) {
+        return calendarEntries.stream()
+                .map(CalendarEntry::date)
+                .reduce(this::minDate)
+                .map(d -> Duration.ofDays(ChronoUnit.DAYS.between(LocalDate.now(), d)))
+                .filter(Duration::isPositive)
+                .orElse(Duration.ofDays(1));
+    }
+
+    private LocalDate minDate(final LocalDate d1, final LocalDate d2) {
+        return d1.isBefore(d2) ? d1 : d2;
     }
 
     private String key(final String street, final String number) {
